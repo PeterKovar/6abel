173b63d215362e6cffdf55a62ea712c24d249146
PeterKovar <kova@htl-donaustadt.at>
2024-05-29 19:48:04 +0200

    Update NeuronalesNetz1.cpp
    Neuronales Netz fertig, forward und sigmod funktione ergänzt

--- NeuronalesNetz1.cpp
+++ NeuronalesNetz1.cpp
@@ -1,6 +1,10 @@
 /*Programm Neuronales Netz
   (c) KOVA 24.04.2024*/
 # include <iostream>
+# include <cmath>
+double sigmod(double s) {
+  return 1.0 / (1.0 + exp(-s));
+}
 //Neuronenklasse
 class Neuron{
   public:
@@ -32,7 +36,7 @@ class NeuronNet{
        //Layers erzeugen
        layers = new Layer [numLayers];
        //Neuronen in den einzelnen Layer erzeugen
-       for (int i=0; i<numLayers; ++i); {
+       for (int i=0; i<numLayers; ++i) {
         layers[i].numNeurons = layer_struc[i]; //Anzahl Neuronen über übernehmen
         layers[i].makeNeurons(); //Neuronen erzeugen
         if (i==0) layers[i].numInputs = 0; //Layer 0 kein Input
@@ -42,8 +46,24 @@ class NeuronNet{
     ~NeuronNet(){
       delete [] layers;   //Speicher freigeben
     }
+    //Vorwaertsfunktion
     void forward (double *input) {
-
+        // Setzen der Werte in den Eingangslayer
+        for (int i=0;i<layers[0].numNeurons; ++i){
+          layers[0].neurons[i].output = input[i];
+        }
+        //Berechnen der Ausgaben für jede Schicht
+        for(int i=1; i < numLayers; ++i) {
+          for(int j=0; j<layers[i].numNeurons; ++j){
+            double sum = 0.0;
+            for (int k=0; k < layers[i].numInputs; ++k) {
+              //Alle Eing. mit dem Gewicht mult. und summieren
+              sum += layers[i-1].neurons[k].output *
+                     layers[i].neurons[j].weight;
+            }
+            layers[i].neurons[j].output = sigmod(sum);
+          }
+        }
     }
      
 };
@@ -52,10 +72,11 @@ class NeuronNet{
 int main(int argc, char **argv) {
 
     int layer_struc[] = {2, 3, 1};
-    std::cout << sizeof(layer_struc)/sizeof(int) << "\n";
+    //std::cout << sizeof(layer_struc)/sizeof(int) << "\n";
     NeuronNet net(layer_struc, sizeof(layer_struc)/sizeof(int));
     double input [] = {0.5, 0.8};
     //Forward durchführen
     net.forward(input);
+    std::cout << net.layers[2].neurons[0].output;
     return 0;
 }
